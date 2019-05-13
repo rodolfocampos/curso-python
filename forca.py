143382cecd40b3f0625ba8454381ad08c35db8c2
Rodolfo Campos <rodolfobc.ti@gmail.com>
2019-05-13 13:55:40 -0300

    recebendo dados e verificando a posicao das letras

--- forca.py
+++ forca.py
@@ -9,6 +9,14 @@ def jogar():
     acertou = False
 
     while not acertou and not enforcou:
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 1
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
         print("Jogando...")
 
 
