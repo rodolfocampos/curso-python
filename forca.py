71235d835872755855f094a5c16d0c5fd9a66a0a
Rodolfo Campos <rodolfobc.ti@gmail.com>
2019-05-13 15:47:55 -0300

    adicionado espaços de acordo com o tamanho da palavra

--- forca.py
+++ forca.py
@@ -3,24 +3,37 @@ def jogar():
     print("Bem vindo ao jogo da forca!\n")
     print("*********************************\n")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "laranja".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
 
+    print("A palavra secreta contem: {}\n".format(letras_acertadas))
     while not acertou and not enforcou:
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                    print("\nEncontrei as letras na posição {}\n".format(letras_acertadas))
+                index += 1
 
-        index = 1
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
-        print("Jogando...")
+            acertou = "_" not in letras_acertadas
+        else:
+            erros += 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+            enforcou = (erros == 6)
 
+    print("\nFim do jogo\n")
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu")
 
-print("Fim do jogo")
 
 if __name__ == "__main__":
     jogar()
